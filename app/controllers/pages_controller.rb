6c6d14b3ab8c4a51872d2235d3add3d01d56b2f4
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2021-02-15 10:59:48 +0100

    lecture rails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,19 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @batch549 = ['daan', 'ludmila', 'antony', 'corey']
+    # @user_info = params["member"]
+    # params[:member]
+    if params["member"]
+      @batch549 = @batch549.select { |student| student == params["member"]}
+    end
   end
 
   def home
   end
 end
+
+
+# PARAMS = {
+  # key: value
+# }
