a3176c3094fc70687d1481419bd84101eae53d40
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2021-02-15 09:57:17 +0100

    add 3 user journeys about/home & contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # our routes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+
+  # get '/home', to: 'pages#home'
+  root to: 'pages#home'
+
+  # GENERIC SYNTAX
+  # verb '/path', to: 'controller_name#method_name'
 end
